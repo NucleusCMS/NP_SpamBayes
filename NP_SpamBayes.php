c3ec2cde313b355f7562893e0d5f334861241562
yamamoto <yamamoto@kyms.jp>
2016-02-12 15:53:28 +0900

    Remove history

--- NP_SpamBayes.php
+++ NP_SpamBayes.php
@@ -5,7 +5,6 @@
  *
  * Bayesian filter for comment and trackback spam
  *
-   ***** BEGIN LICENSE BLOCK *****
 
    The Initial Developer of the Original Code is
    Loic d'Anterroches [loic_at_xhtml.net].
@@ -32,25 +31,6 @@
    the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
    in which case the provisions of the LGPL are applicable instead
    of those above.
-
-  ***** END LICENSE BLOCK *****
-
-  ***** Version history *****
-  Version 1.0   : 2006 09 06 Stable on development and fresh installed blog.
-          1.0.1 : 2006 09 11 NAN bug solved, some more information on the screens
-          1.0.2 : 2006 09 15 Logging filtering applied to both ham and spam as well as different logtypes. Handy when
-          					 a lot of plugins use spambaues as a spam filter.
-          1.0.3 : 2006 09 19 Logging now adherse the plugin option setting (thanks VJ)
-          					 Added the feature to train all 'new' comments
-          1.0.4 : 2006 09 26 Logging now adherse the plugin option setting also in version 4 of PHP (thanks pepiino)
-          1.0.5 : 2006 10 15 Update probabilities now made obsolete. The function is run after all training sessions.
-		  1.1.0 Beta 2007 01 07 Logger functions have been enhanched dramaticly.
-		  					 Items per page is now a user setting.
-		  					 It's possible to scan for keywords inside the content
-		  					 Explain functionality to see how a logged event scores against SpamBayes keywords. Prints both ham and spam results.
-		  1.1.0	2007 01 08 	 Promote to weblog. Comments only. Will teach the document a s Ham and publishes the logged event as a legit comment.
-		  					 Pagecounter could be wrong..
-  ***** End version history *****
 
  * based on: many sources:
  * http://priyadi.net/archives/2005/10/07/wpbayes-naive-bayesian-comment-spam-filter-for-wordpress/
